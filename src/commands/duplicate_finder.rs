423a73a931afc1b7831fc2e7ca668ff51bc1e5fb
Skippy <steelstone3@gmail.com>
2024-12-05 21:46:02 +0000

    version-05-12-24

--- src/commands/duplicate_finder.rs
+++ src/commands/duplicate_finder.rs
@@ -6,12 +6,13 @@ impl DuplicateFinder {
             Message::MenuBar => {}
             Message::SelectedTabChanged(tab_identifier) => self.switch_tab(tab_identifier),
             Message::FileNew => *self = DuplicateFinder::default(),
-            Message::EditUndo => todo!(),
-            Message::EditRedo => todo!(),
-            Message::ViewPrependLineNumbersPressed => self.prepend_line_numbers(),
+            Message::EditUndo => {}
+            Message::EditRedo => {}
+            Message::ViewPrependLineNumbersPressed => self.toggle_prepend_line_numbers(),
             Message::ViewThemeChanged => self.switch_theme(),
             Message::EditorChanged(action) => self.application_state.content.perform(action),
             Message::DuplicateSearchPressed => {
+                self.remove_prepend();
                 self.find_duplicates();
             }
         }
